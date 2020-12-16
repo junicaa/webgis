0da91c68264416608ded60d1722e616a3df38908
junicaa <76077382+junicaa@users.noreply.github.com>
2020-12-16 07:27:02 +0700

    Add files via upload

---
+++ data/titikacuan_1.js
@@ -0,0 +1,8 @@
+var json_titikacuan_1 = {
+"type": "FeatureCollection",
+"name": "titikacuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.409309234466633, -6.984325846449203 ] } }
+]
+}
